0f82b5d358618515736b76f1ef1299007c386971
sabudou <sabudou@gmail.com>
2020-12-08 20:46:56 -0600

    new updates html

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ engine = create_engine("sqlite:///shelters.sqlite")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("template_index.html")
 
 @app.route("/api/shelters")
 def shelters():
-    print(pd.read_sql_table("national", engine).to_json(orient="records"))
+    #print(pd.read_sql_table("national", engine).to_json(orient="records"))
     return pd.read_sql_table("national", engine).to_json(orient="records")
 
 if __name__ == "__main__":
